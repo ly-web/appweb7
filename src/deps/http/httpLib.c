d759d341d8d72a90c8270152f56712bf3e57c758
Embedthis Software <dev@embedthis.com>
2012-10-26 20:21:47 -0700

    Restore testing HttpConn.sock in httpFinalize

--- src/deps/http/httpLib.c
+++ src/deps/http/httpLib.c
@@ -14135,7 +14135,7 @@ PUBLIC void httpComplete(HttpConn *conn)
         return;
     }
     tx->complete = 1;
-    if (conn->state < HTTP_STATE_CONNECTED || !conn->writeq) {
+    if (conn->state < HTTP_STATE_CONNECTED || !conn->writeq || !conn->sock) {
         /* Tx Pipeline not yet created */
         tx->pendingCompletion = 1;
         return;
@@ -14157,7 +14157,7 @@ PUBLIC void httpFinalize(HttpConn *conn)
     if (!tx || tx->finalized) {
         return;
     }
-    if (conn->state >= HTTP_STATE_CONNECTED && conn->writeq) {
+    if (conn->state >= HTTP_STATE_CONNECTED && conn->writeq && conn->sock) {
         tx->responded = 1;
         tx->finalized = 1;
         httpPutForService(conn->writeq, httpCreateEndPacket(), HTTP_SCHEDULE_QUEUE);
