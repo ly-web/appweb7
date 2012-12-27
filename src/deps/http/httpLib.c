4d0228920bdb7246471c07671a56a169d5e458c1
Embedthis Software <dev@embedthis.com>
2012-12-27 14:07:11 -0800

    Always define BIT_64

--- src/deps/http/httpLib.c
+++ src/deps/http/httpLib.c
@@ -5701,7 +5701,7 @@ static void netOutgoingService(HttpQueue *q)
                 httpSocketBlocked(conn);
                 break;
             }
-            if (errCode != EPIPE && errCode != ECONNRESET && errCode != ENOTCONN) {
+            if (errCode != EPIPE && errCode != ECONNRESET && errCode != ECONNABORTED && errCode != ENOTCONN) {
                 httpError(conn, HTTP_ABORT | HTTP_CODE_COMMS_ERROR, "netConnector: Write response error %d", errCode);
             } else {
                 httpDisconnect(conn);
@@ -13205,7 +13205,7 @@ PUBLIC void httpSendOutgoingService(HttpQueue *q)
             /*  Socket full, wait for an I/O event */
             httpSocketBlocked(conn);
         } else {
-            if (errCode != EPIPE && errCode != ECONNRESET && errCode != ENOTCONN) {
+            if (errCode != EPIPE && errCode != ECONNRESET && errCode != ECONNABORTED && errCode != ENOTCONN) {
                 httpError(conn, HTTP_ABORT | HTTP_CODE_COMMS_ERROR, "sendConnector: error, errCode %d", errCode);
             } else {
                 httpDisconnect(conn);
