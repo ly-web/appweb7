f8cbc79460a8807e1aa81804bbfc0df0fed2805f
Embedthis Software <dev@embedthis.com>
2013-04-08 16:39:10 -0700

    Update simple-client sample

---
+++ samples/simple-client/README.md
@@ -0,0 +1,30 @@
+Simple Client Sample
+===
+
+This sample shows how to use the Http library to issue Http client requests.
+
+Requirements
+---
+* [Appweb](http://embedthis.com/downloads/appweb/download.ejs)
+* [Bit Build Tool](http://embedthis.com/downloads/bit/download.ejs)
+
+To build:
+---
+    bit 
+
+To run:
+---
+    bit run
+
+The client retrieves:
+ 
+     http://www.embedthis.com/index.html
+
+Code:
+---
+* [client.c](client.c) - Main program
+* [start.bit](start.bit) - Bit build instructions
+
+See Also:
+---
+* [simple-server - Simple server and embedding API](../simple-server/README.md)
--- samples/simple-client/client.c
+++ samples/simple-client/client.c
@@ -1,8 +1,7 @@
 /*
-    simpleClient.c - Simple client using the GET method to retrieve a web page.
+    client.c - Simple client using the GET method to retrieve a web page.
   
-    This sample demonstrates retrieving content using the HTTP GET 
-    method via the Client class. This is a multi-threaded application.
+    This sample demonstrates retrieving content using the HTTP GET method.
   
     Copyright (c) All Rights Reserved. See copyright notice at the bottom of the file.
  */
@@ -10,62 +9,50 @@
 
 #include    "appweb.h"
 
-/****************************** Forwards ******************************/
-
-typedef struct App {
-    Mpr         *mpr;
-    Http        *http;
-    HttpConn    *conn;
-} App;
-
-static void manageApp(App *app, int flags);
-
 /********************************* Code *******************************/
 
 MAIN(simpleClient, int argc, char **argv, char **envp)
 {
-    Mpr     *mpr;
-    App     *app;
-    cchar   *content;
-    int     code;
+    Http        *http;
+    HttpConn    *conn;
+    cchar       *content;
+    int         code;
 
     /* 
-       Create the Multithreaded Portable Runtime
+       Create the Multithreaded Portable Runtime and start it.
      */
-    mpr = mprCreate(argc, argv, MPR_USER_EVENTS_THREAD);
-    if ((app = mprAllocObj(App, manageApp)) == 0) {
-        return MPR_ERR_MEMORY;
-    }
-    mprAddRoot(app);
-    mprAddStandardSignals(app);
-
-    /* 
-       Start the Multithreaded Portable Runtime
-     */
+    mprCreate(argc, argv, 0);
     mprStart();
 
     /* 
-       Create an Http service object
-     */
-    app->http = httpCreate(HTTP_CLIENT_SIDE);
-
-    /* 
        Get a client http object to work with. We can issue multiple requests with this one object.
+       Add the conn as a root object so the GC won't collect it while we are using it.
      */
-    app->conn = httpCreateConn(app->http, NULL, NULL);
+    http = httpCreate(HTTP_CLIENT_SIDE);
+    conn = httpCreateConn(http, NULL, NULL);
+    mprAddRoot(conn);
 
     /* 
-       Get a URL
+       Open a connection to issue the GET. Then finalize the request output - this forces the request out.
      */
-    if (httpConnect(app->conn, "GET", "http://www.embedthis.com/index.html", NULL) < 0) {
+    if (httpConnect(conn, "GET", "http://www.embedthis.com/index.html", NULL) < 0) {
         mprError("Can't get URL");
+        exit(2);
+    }
+    httpFinalizeOutput(conn);
+
+    /*
+        Wait for a response
+     */
+    if (httpWait(conn, HTTP_STATE_PARSED, 10000) < 0) {
+        mprError("No response");
         exit(2);
     }
 
     /* 
        Examine the HTTP response HTTP code. 200 is success.
      */
-    code = httpGetStatus(app->conn);
+    code = httpGetStatus(conn);
     if (code != 200) {
         mprError("Server responded with code %d\n", code);
         exit(1);
@@ -74,7 +61,7 @@ MAIN(simpleClient, int argc, char **argv, char **envp)
     /* 
        Get the actual response content
      */
-    content = httpReadString(app->conn);
+    content = httpReadString(conn);
     if (content) {
         mprPrintf("Server responded with: %s\n", content);
     }
@@ -82,16 +69,6 @@ MAIN(simpleClient, int argc, char **argv, char **envp)
     return 0;
 }
 
-
-/*
-    Manage the app instance for the garbage collector
- */
-static void manageApp(App *app, int flags)
-{
-    if (flags & MPR_MANAGE_MARK) {
-        mprMark(app);
-    }
-}
 
 /*
     @copy   default
