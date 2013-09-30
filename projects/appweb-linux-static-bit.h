19c1d491ff2bd40d337e779252867f614992b93a
Embedthis Software <dev@embedthis.com>
2013-09-30 09:40:11 -0700

    Regen

--- projects/appweb-linux-static-bit.h
+++ projects/appweb-linux-static-bit.h
@@ -167,7 +167,7 @@
     #define BIT_TUNE "size"
 #endif
 #ifndef BIT_VERSION
-    #define BIT_VERSION "4.4.0"
+    #define BIT_VERSION "4.4.1"
 #endif
 #ifndef BIT_WARN64TO32
     #define BIT_WARN64TO32 0
@@ -196,7 +196,7 @@
     #define BIT_APP_PREFIX "/usr/local/lib/appweb"
 #endif
 #ifndef BIT_VAPP_PREFIX
-    #define BIT_VAPP_PREFIX "/usr/local/lib/appweb/4.4.0"
+    #define BIT_VAPP_PREFIX "/usr/local/lib/appweb/4.4.1"
 #endif
 #ifndef BIT_BIN_PREFIX
     #define BIT_BIN_PREFIX "/usr/local/bin"
@@ -229,7 +229,7 @@
     #define BIT_CACHE_PREFIX "/var/spool/appweb/cache"
 #endif
 #ifndef BIT_SRC_PREFIX
-    #define BIT_SRC_PREFIX "appweb-4.4.0"
+    #define BIT_SRC_PREFIX "appweb-4.4.1"
 #endif
 
 /* Suffixes */
@@ -271,10 +271,10 @@
     #define BIT_MINOR_VERSION 4
 #endif
 #ifndef BIT_PATCH_VERSION
-    #define BIT_PATCH_VERSION 0
+    #define BIT_PATCH_VERSION 1
 #endif
 #ifndef BIT_VNUM
-    #define BIT_VNUM 400040000
+    #define BIT_VNUM 400040001
 #endif
 
 /* Packs */
