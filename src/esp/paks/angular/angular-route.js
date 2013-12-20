5d0e8cc7756f5fa77f12f56ec54f7dce4294cf2b
Embedthis Software <dev@embedthis.com>
2013-12-20 14:42:40 -0800

    UPDATE: Angular 1.2.6

--- src/esp/paks/angular/angular-route.js
+++ src/esp/paks/angular/angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.5
+ * @license AngularJS v1.2.6
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -909,3 +909,4 @@ function ngViewFillContentFactory($compile, $controller, $route) {
 
 
 })(window, window.angular);
+
