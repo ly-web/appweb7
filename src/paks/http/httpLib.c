947b1603d1c0c568b7ee880681797f9a7100b6e0
Embedthis Software <dev@embedthis.com>
2014-01-02 11:23:33 -0800

    FIX: web socket route for ESP applications when hosted.

--- src/paks/http/httpLib.c
+++ src/paks/http/httpLib.c
@@ -11677,15 +11677,14 @@ PUBLIC void httpAddHomeRoute(HttpRoute *parent)
 PUBLIC void httpAddWebSocketsRoute(HttpRoute *parent, cchar *prefix, cchar *name)
 {
     HttpRoute   *route;
-    cchar       *path, *pattern;
+    cchar       *pattern;
 
     if (parent->prefix) {
         prefix = sjoin(parent->prefix, prefix, NULL);
         name = sjoin(parent->prefix, name, NULL);
     }
     pattern = sfmt("^%s/{controller}/stream", prefix);
-    path = mprGetRelPath(stemplate("$1-cmd-stream", parent->vars), parent->documents);
-    route = httpDefineRoute(parent, name, "GET", pattern, path, "${controller}.c");
+    route = httpDefineRoute(parent, name, "GET", pattern, "$1-cmd-stream", "${controller}.c");
     httpAddRouteFilter(route, "webSocketFilter", "", HTTP_STAGE_RX | HTTP_STAGE_TX);
 }
 
