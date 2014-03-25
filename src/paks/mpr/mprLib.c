e27890e5bb1150ccbd5bb819a6296d5b9c996ef0
Embedthis Software <dev@embedthis.com>
2014-03-25 12:49:15 -0700

    FIX: mprGetCPU for linux

--- src/paks/mpr/mprLib.c
+++ src/paks/mpr/mprLib.c
@@ -2197,7 +2197,7 @@ PUBLIC size_t mprGetMem()
 
 PUBLIC uint64 mprGetCPU()
 {
-    uint64     ticks, utime, stime;
+    uint64     ticks;
 
     ticks = 0;
 #if LINUX
@@ -2205,23 +2205,18 @@ PUBLIC uint64 mprGetCPU()
     char path[ME_MAX_PATH];
     sprintf(path, "/proc/%d/stat", getpid());
     if ((fd = open(path, O_RDONLY)) >= 0) {
-        char buf[ME_MAX_BUFFER], *tok;
+        char buf[ME_MAX_BUFFER];
         int nbytes = read(fd, buf, sizeof(buf) - 1);
         close(fd);
         if (nbytes > 0) {
+            ulong utime, stime;
             buf[nbytes] = '\0';
-            if ((tok = strstr(buf, "utime:")) != 0) {
-                for (tok += 6; tok && isspace((uchar) *tok); tok++) {}
-                utime = stoi(tok);
-            }
-            if ((tok = strstr(buf, "stime:")) != 0) {
-                for (tok += 6; tok && isspace((uchar) *tok); tok++) {}
-                stime = stoi(tok);
-            }
+            sscanf(buf, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu", &utime, &stime);
             ticks = (utime + stime) * MPR_TICKS_PER_SEC / sysconf(_SC_CLK_TCK);
         }
     }
 #elif MACOSX
+    uint64 utime, stime;
     struct task_thread_times_info info;
     mach_msg_type_number_t count = sizeof(info);
     if (task_info(mach_task_self(), TASK_THREAD_TIMES_INFO, (task_info_t) &info, &count) == KERN_SUCCESS) {
