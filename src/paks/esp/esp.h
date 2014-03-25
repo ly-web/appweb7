46a004e3e599f8f4abe078bfc24ee78e2ae87fd6
Embedthis Software <dev@embedthis.com>
2014-03-24 20:27:11 -0700

    DOC: Fix httpUri references in ESP doc.

--- src/paks/esp/esp.h
+++ src/paks/esp/esp.h
@@ -4363,7 +4363,7 @@ PUBLIC void espRefresh(HttpConn *conn, cchar *on, cchar *off, cchar *options);
 
 /**
     Render a script link.
-    @param uri Script URI to load. Set to null to get a default set of scripts. See #httpUri for a list of possible
+    @param uri Script URI to load. Set to null to get a default set of scripts. See #httpLink for a list of possible
         URI formats.
     @param conn Http connection object
     @param options Extra options. See \l EspControl \el for a list of the standard options.
@@ -4375,7 +4375,7 @@ PUBLIC void espScript(HttpConn *conn, cchar *uri, cchar *options);
 
 /**
     Render a stylesheet link
-    @param uri Stylesheet URI to load. Set to null to get a default set of stylesheets. See #httpUri for a list of possible
+    @param uri Stylesheet URI to load. Set to null to get a default set of stylesheets. See #httpLink for a list of possible
         URI formats.
     @param conn Http connection object
     @param options Extra options. See \l EspControl \el for a list of the standard options.
@@ -4705,7 +4705,7 @@ PUBLIC void refresh(cchar *on, cchar *off, cchar *options);
 
 /**
     Render a script link
-    @param uri Script URI to load. Set to null to get a default set of scripts. See #httpUri for a list of possible
+    @param uri Script URI to load. Set to null to get a default set of scripts. See #httpLink for a list of possible
         URI formats.
     @param options Extra options. See \l EspControl \el for a list of the standard options.
     @ingroup EspAbbrev
@@ -4717,7 +4717,7 @@ PUBLIC void script(cchar *uri, cchar *options);
 /**
     Render a stylesheet link
     @param uri Stylesheet URI to load. Set to null to get a default set of stylesheets. 
-        See #httpUri for a list of possible URI formats.
+        See #httpLink for a list of possible URI formats.
     @param options Extra options. See \l EspControl \el for a list of the standard options.
     @ingroup EspAbbrev
     @stability Deprecated
