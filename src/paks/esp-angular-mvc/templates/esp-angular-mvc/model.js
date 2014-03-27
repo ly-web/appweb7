1b6f346293bf43738b2c15a39e163e569a9692f1
Embedthis Software <dev@embedthis.com>
2014-03-27 13:07:51 -0700

    CLEANUP: paks
    Remove angular, legacy-mvc

--- src/paks/esp-angular-mvc/templates/esp-angular-mvc/model.js
+++
@@ -1,8 +0,0 @@
-/*
-    ${UMODEL}.js - ${UMODEL} model (esp-angular-mvc)
- */
-'use strict';
-
-angular.module('app').factory('${UMODEL}', function (EspResource) {
-    return EspResource.group("${MODEL}");
-});
