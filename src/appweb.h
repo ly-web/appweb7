a200ded9e7d2e4fef9224e39a812647cd570a3e9
Embedthis Software <dev@embedthis.com>
2014-04-06 23:55:39 -0700

    DOC: Update doc.

--- src/appweb.h
+++ src/appweb.h
@@ -605,7 +605,7 @@ PUBLIC bool maTokenize(MaState *state, cchar *str, cchar *fmt, ...);
     @param tok Next token reference
     @return Reference to the next token. (Not allocate
     @ingroup MaAppweb
-    @stability Prototype
+    @stability Evolving
 */
 PUBLIC char *maGetNextArg(char *s, char **tok);
 
