189ffef2abe3e8f1456704cf3ffc321730631e0a
Embedthis Software <dev@embedthis.com>
2014-05-26 22:08:39 -0700

    FIX: disable xsrf in http routes by default. Enable in esp route sets

--- src/paks/esp/espLib.c
+++ src/paks/esp/espLib.c
@@ -2598,6 +2598,7 @@ static void serverRouteSet(HttpRoute *parent, cchar *set)
     HttpRoute   *route;
 
     /* Simple controller/action route */
+    httpSetRouteXsrf(parent, 1);
     route = httpAddRestfulRoute(parent, parent->serverPrefix, "action", "GET,POST","/{action}(/)*$",
         "${action}", "{controller}");
     httpAddRouteHandler(route, "espHandler", "");
@@ -2606,6 +2607,7 @@ static void serverRouteSet(HttpRoute *parent, cchar *set)
 
 static void angularRouteSet(HttpRoute *parent, cchar *set)
 {
+    httpSetRouteXsrf(parent, 1);
     httpAddRouteHandler(parent, "espHandler", "");
     httpAddWebSocketsRoute(parent, 0, "/*/stream");
     httpAddResourceGroup(parent, 0, "{controller}");
@@ -2616,6 +2618,7 @@ static void angularRouteSet(HttpRoute *parent, cchar *set)
 
 static void htmlRouteSet(HttpRoute *parent, cchar *set)
 {
+    httpSetRouteXsrf(parent, 1);
     httpAddRouteHandler(parent, "espHandler", "");
     httpDefineRoute(parent,
         sfmt("%s%s/*", parent->prefix, parent->serverPrefix), 
--- src/paks/http/httpLib.c
+++ src/paks/http/httpLib.c
@@ -516,7 +516,7 @@ PUBLIC void httpSetAuthOrder(HttpAuth *auth, int order)
 
 
 /*
-    Form login service routine. Called in response to a form-based login request.
+    Form login service routine. Called in response to a form-based login request. Only used when httpSetAuthForm is utilized.
     The password is clear-text so this must be used over SSL to be secure.
  */
 static void loginServiceProc(HttpConn *conn)
@@ -9934,7 +9934,7 @@ PUBLIC HttpRoute *httpCreateRoute(HttpHost *host)
     route->auth = httpCreateAuth();
     route->defaultLanguage = sclone("en");
     route->home = route->documents = mprGetCurrentPath();
-    route->flags = HTTP_ROUTE_STEALTH | HTTP_ROUTE_XSRF;
+    route->flags = HTTP_ROUTE_STEALTH;
 #if ME_DEBUG
     route->flags |= HTTP_ROUTE_SHOW_ERRORS;
     route->keepSource = 1;
@@ -10480,8 +10480,9 @@ static int checkRoute(HttpConn *conn, HttpRoute *route)
             }
         }
     }
-    httpSetParam(conn, "prefix", route->prefix);
-
+    if (route->prefix[0]) {
+        httpSetParam(conn, "prefix", route->prefix);
+    }
     if ((rc = selectHandler(conn, route)) != HTTP_ROUTE_OK) {
         return rc;
     }
