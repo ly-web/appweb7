8655052acebffabed353e6c44e36bec591662c99
Embedthis Software <dev@embedthis.com>
2015-03-24 12:27:15 -0700

    FIX: saving headers for error documents.

--- paks/http/dist/httpLib.c
+++ paks/http/dist/httpLib.c
@@ -16146,7 +16146,7 @@ static bool parseRequestLine(HttpConn *conn, HttpPacket *packet)
 {
     HttpRx      *rx;
     HttpLimits  *limits;
-    char        *method, *uri, *protocol, *start;
+    char        *headers, *method, *uri, *protocol, *start;
     MprBuf      *content;
     ssize       len;
 
@@ -16161,6 +16161,8 @@ static bool parseRequestLine(HttpConn *conn, HttpPacket *packet)
 
     content = packet->content;
     start = content->start;
+    headers = httpTracing(conn) ? snclone(start, rx->headerPacketLength) : 0;
+
     method = getToken(conn, 0);
     rx->originalMethod = rx->method = supper(method);
     parseMethod(conn);
@@ -16199,11 +16201,7 @@ static bool parseRequestLine(HttpConn *conn, HttpPacket *packet)
     if (httpTracing(conn)) {
         httpTrace(conn, "rx.first.server", "request", "method:'%s',uri:'%s',protocol:'%s'", 
             rx->method, rx->uri, conn->protocol);
-        uri[-1] = ' ';
-        protocol[-1] = ' ';
-        content->start[-2] = '\r';
-        content->start[-1] = '\n';
-        httpTraceContent(conn, "rx.headers.server", "context", start, rx->headerPacketLength, NULL);
+        httpTraceContent(conn, "rx.headers.server", "context", headers, rx->headerPacketLength, NULL);
     }
     return 1;
 }
