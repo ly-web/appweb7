4b4d8ed98ee471e21b29e3c928a229e180a4b857
Embedthis Software <dev@embedthis.com>
2015-04-23 12:51:02 -0700

    FIX: route display

--- src/http/httpLib.c
+++ src/http/httpLib.c
@@ -8911,7 +8911,7 @@ static void printRoute(HttpRoute *route, int idx, bool full, int methodsLen, int
     cchar       *methods, *pattern, *target, *index;
     int         nextIndex;
 
-    if (route->flags & HTTP_ROUTE_HIDDEN) {
+    if (route->flags & HTTP_ROUTE_HIDDEN && !full) {
         return;
     }
     auth = route->auth;
@@ -8925,7 +8925,7 @@ static void printRoute(HttpRoute *route, int idx, bool full, int methodsLen, int
         if (route->prefix && *route->prefix) {
             printf("    Prefix:       %s\n", route->prefix);
         }
-        printf("    RegExp:       %s\n", route->optimizedPattern);
+        printf("    RegExp:       %s\n", route->optimizedPattern ? route->optimizedPattern : "");
         printf("    Methods:      %s\n", methods);
         printf("    Target:       %s\n", target);
         printf("    Auth:         %s\n", auth->type ? auth->type->name : "-");
