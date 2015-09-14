dda974b51aabc079a12cfe51b6a6625fe4924f86
Embedthis Software <dev@embedthis.com>
2015-09-14 13:02:22 -0700

    FIX: ESP mdbReadWhere when index is zero.

--- src/esp/espLib.c
+++ src/esp/espLib.c
@@ -5985,7 +5985,7 @@ PUBLIC bool espCompile(HttpRoute *route, MprDispatcher *dispatcher, cchar *sourc
         if (runCommand(route, dispatcher, eroute->link, csource, module, errMsg) != 0) {
             return 0;
         }
-#if !(ME_DEBUG && MACOSX)
+#if !MACOSX
         /*
             MAC needs the object for debug information
          */
@@ -7579,7 +7579,7 @@ static EdiGrid *mdbReadWhere(Edi *edi, cchar *tableName, cchar *columnName, ccha
             return 0;
         }
         if (col->flags & EDI_INDEX && (op == OP_EQ)) {
-            if ((r = lookupRow(table, value)) != 0) {
+            if ((r = lookupRow(table, value)) >= 0) {
                 row = getRow(table, r);
                 grid->records[0] = createRecFromRow(edi, row);
                 grid->nrecords = 1;
