88a125bea581ac0c750eea5e6771fa6d041ab5ac
Embedthis Software <dev@embedthis.com>
2015-10-09 20:00:35 -0700

    REGEN: doc and projects

--- projects/appweb-vxworks-default-me.h
+++ projects/appweb-vxworks-default-me.h
@@ -178,7 +178,7 @@
     #define ME_TUNE "size"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "6.1.0"
+    #define ME_VERSION "6.1.1"
 #endif
 #ifndef ME_WATCHDOG_NAME
     #define ME_WATCHDOG_NAME "appman"
@@ -240,7 +240,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/appweb-6.1.0"
+    #define ME_SRC_PREFIX "/usr/src/appweb-6.1.1"
 #endif
 
 /* Suffixes */
@@ -282,10 +282,10 @@
     #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 600010000
+    #define ME_VNUM 600010001
 #endif
 
 /* Components */
