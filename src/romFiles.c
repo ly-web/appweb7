da0240d241738bda313255d80d28bfbd39887ff5
Embedthis Software <dev@embedthis.com>
2015-11-13 17:05:44 -0800

    DOC

--- src/romFiles.c
+++ src/romFiles.c
@@ -1,15 +1,26 @@
 /*
     romFiles -- Compiled Files
+
+    Generate via:
+
+    makerom --prefix / files...
  */
 #include "mpr.h"
 
 #if ME_ROM
 
+static uchar _file_1[] = {
+    0
+};
+
 PUBLIC MprRomInode romFiles[] = {
-    { "", 0, 0, 0 },
+    { "/appweb.conf", _file_1, 3401, 1 },
     { 0, 0, 0, 0 },
 };
 
+PUBLIC MprRomInode *mprGetRomFiles() {
+    return romFiles;
+}
 #else
 PUBLIC int romDummy;
 #endif /* ME_ROM */
