706dfa6f3ebf7eb3c619843a0b4e8a20b7cb1561
Embedthis Software <dev@embedthis.com>
2015-11-27 12:34:31 -0800

    FIX: TraceLog with ROM

--- src/config.c
+++ src/config.c
@@ -229,7 +229,6 @@ static int parseFileInner(MaState *state, cchar *path)
 }
 
 
-#if !ME_ROM
 /*
     TraceLog path|-
         [size=bytes]
@@ -308,7 +307,6 @@ static int traceLogDirective(MaState *state, cchar *key, cchar *value)
     httpSetTraceLevel(level);
     return 0;
 }
-#endif
 
 
 /*
